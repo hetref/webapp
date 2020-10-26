2dc007706651f1de578498055c13110dcd9a2e96
hetref <70653460+hetref@users.noreply.github.com>
2020-10-26 11:39:05 +0530

    my database web app

---
+++ index.js
@@ -0,0 +1,16 @@
+const userID = document.getElementById("userID");
+const firstName = document.getElementById("firstName");
+const lastName = document.getElementById("lastName");
+const age = document.getElementById("age");
+const addBtn = document.getElementById("addBtn");
+
+const database = firebase.database();
+
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    database.ref('/users' + userID.value).set({
+        first_name: firstName.value,
+        last_name: lastName.value,
+        age: age.value
+    });
+});
